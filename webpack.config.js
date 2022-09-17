e386cc9498aa6eb5327629418ef37953e7af0be5
Andy Glover <andrew.l.glover@gmail.com>
2022-09-16 22:32:47 -0700

    Add entry for pageload.js and rename outputs to [name].bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        pageload: './src/pageload.js'
+    },
     devServer: {
         static:  './dist',
     },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
