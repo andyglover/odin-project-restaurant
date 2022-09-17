22038e759d25952347771dd4343d69547193a392
Andy Glover <andrew.l.glover@gmail.com>
2022-09-16 22:46:10 -0700

    Move heading and paragraph from template.html to pageload.js

--- src/pageload.js
+++ src/pageload.js
@@ -1,17 +1,25 @@
 import Restaurant from './restaurant.jpg';
 
 export default function pageload() {    
-        console.log('This stuff happens when the page is loaded!');
+    console.log('This stuff happens when the page is loaded!');
 
-        function component() {
+    function component() {
         const element = document.createElement('div');
         //add the restaurant image
         const myRestaurant = new Image();
         myRestaurant.src = Restaurant;
-
         element.appendChild(myRestaurant);
+        //Add the H1
+        const heading = document.createElement('h1');
+        heading.innerText = "This is a restaurant!";
+        element.appendChild(heading);
+        //Add the paragraph
+        const paragraph = document.createElement('p');
+        paragraph.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolorum ex necessitatibus quis, voluptatem architecto pariatur ipsa saepe eos nobis reiciendis minus eligendi dolorem qui!"
+        element.appendChild(paragraph);
 
         return element;
     }
+
     document.getElementById("content").appendChild(component());
 }
