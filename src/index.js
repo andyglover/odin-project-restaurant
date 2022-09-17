25ce8c5145e54b29209f3c4bb1401417d9dad656
Andy Glover <andrew.l.glover@gmail.com>
2022-09-16 23:27:04 -0700

    Wrap tab render functions

--- src/index.js
+++ src/index.js
@@ -5,37 +5,55 @@ pageload();
 //make the tabs
 const content = document.getElementById("content");
 
+const clearContent = () => {
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+}
+
 const tabHome = (() => {
-    const tabHome = document.createElement('p');
-    tabHome.innerText = "home";
     
-    const showContent = function () {
+    const showContent = () => {
+        clearContent();
         console.log("Showing home tab")
     }
+
+    const render = (() => {
+    const tabHome = document.createElement('p');
+    tabHome.innerText = "home";
     tabHome.addEventListener("click", showContent)
     content.appendChild(tabHome);
+    })();
+    
 })();
 
 const tabMenu = (() => {
-    const tabMenu = document.createElement('p');
-    tabMenu.innerText = "menu";
+    
     
     const showContent = () => {
+        clearContent();
         console.log("Showing menu tab")
     }
-    tabMenu.addEventListener("click", showContent)
-    content.appendChild(tabMenu);
+    
+    const render = (() => {
+        const tabMenu = document.createElement('p');
+        tabMenu.innerText = "menu";
+        tabMenu.addEventListener("click", showContent)
+        content.appendChild(tabMenu);
+    })();
 })();
 
 const tabContact = (() => {
-    const tabContact = document.createElement('p');
-    tabContact.innerText = "contact";
-    
     const showContent = () => {
+        clearContent();
         console.log("Showing contact tab")
     }
-    tabContact.addEventListener("click", showContent)
-    content.appendChild(tabContact);
+    const render = (() => {
+        const tabContact = document.createElement('p');
+        tabContact.innerText = "contact";
+        tabContact.addEventListener("click", showContent)
+        content.appendChild(tabContact);
+    })();
 })();
 
 console.log("test2");
