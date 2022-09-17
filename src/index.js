ca588286af3fe366d120c25a560764267a37f1d8
Andy Glover <andrew.l.glover@gmail.com>
2022-09-16 23:32:00 -0700

    Make tab click rerender tabs

--- src/index.js
+++ src/index.js
@@ -10,50 +10,62 @@ const clearContent = () => {
         content.removeChild(content.lastChild);
     }
 }
+const renderTabs = () => {
+    tabHome.render();
+    tabMenu.render();
+    tabContact.render();
+}
 
 const tabHome = (() => {
     
     const showContent = () => {
         clearContent();
+        renderTabs();
         console.log("Showing home tab")
     }
 
-    const render = (() => {
+    const render = () => {
     const tabHome = document.createElement('p');
     tabHome.innerText = "home";
     tabHome.addEventListener("click", showContent)
     content.appendChild(tabHome);
-    })();
-    
+    };
+    render();
+    return { render };
 })();
 
 const tabMenu = (() => {
-    
-    
+
     const showContent = () => {
         clearContent();
+        renderTabs();
         console.log("Showing menu tab")
     }
     
-    const render = (() => {
+    const render = () => {
         const tabMenu = document.createElement('p');
         tabMenu.innerText = "menu";
         tabMenu.addEventListener("click", showContent)
         content.appendChild(tabMenu);
-    })();
+    };
+    render();
+    return { render };
 })();
 
 const tabContact = (() => {
     const showContent = () => {
         clearContent();
+        renderTabs();
         console.log("Showing contact tab")
     }
-    const render = (() => {
+    const render = () => {
         const tabContact = document.createElement('p');
         tabContact.innerText = "contact";
         tabContact.addEventListener("click", showContent)
         content.appendChild(tabContact);
-    })();
+    };
+    render();
+    return { render };
 })();
 
 console.log("test2");
