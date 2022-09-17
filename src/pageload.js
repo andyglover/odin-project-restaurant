b25b62cfe0b41be46537946e5ac9cd0b5a55d36e
Andy Glover <andrew.l.glover@gmail.com>
2022-09-16 20:45:41 -0700

    Add image load to js

--- src/pageload.js
+++ src/pageload.js
@@ -1,3 +1,17 @@
-export default function pageload() {
+import Restaurant from './restaurant.jpg';
+
+export default function pageload() {    
     console.log('This stuff happens when the page is loaded!');
+
+    function component() {
+    const element = document.createElement('div');
+    //add the restaurant image
+    const myRestaurant = new Image();
+    myRestaurant.src = Restaurant;
+
+    element.appendChild(myRestaurant);
+
+    return element;
+    }
+    document.body.appendChild(component());
 }
