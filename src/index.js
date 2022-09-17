8f322c67646ca823dc0328162c1891e2230ef15c
Andy Glover <andrew.l.glover@gmail.com>
2022-09-16 22:59:20 -0700

    put tabs in modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import pageload from './pageload.js';
 console.log("Hello, Webpack!");
 pageload();
+
+//make the tabs
+const content = document.getElementById("content");
+
+const tabHome = (() => {
+    const tabHome = document.createElement('p');
+    tabHome.innerText = "home";
+    content.appendChild(tabHome);
+})();
+
+const tabMenu = (() => {
+    const tabMenu = document.createElement('p');
+    tabMenu.innerText = "menu";
+    content.appendChild(tabMenu);
+})();
+
+const tabContact = (() => {
+    const tabContact = document.createElement('p');
+    tabContact.innerText = "contact";
+    content.appendChild(tabContact);
+})();
